913851ebb8c4a36d2b4035e39a839a5b31f50018
anastasia-Bella <serenitty99@gmail.com>
2025-10-02 08:28:04 +0000

    ajout artefact

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -18,6 +18,7 @@ def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     y = features[target]
     model = RandomForestRegressor(n_estimators=1, max_depth=10, n_jobs=1)
     model.fit(X, y)
+    # test de l'ajout avec ce format
     timestamp_str = time.strftime('%Y%m%d-%H%M%S')
     joblib.dump(model, os.path.join(model_registry_folder, timestamp_str + '.joblib'))
 
