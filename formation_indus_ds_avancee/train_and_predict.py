a480b72014ec6bd580128e67d457fe93f085d939
anastasia-Bella <serenitty99@gmail.com>
2025-10-02 08:30:39 +0000

    correction suite a erreur flake8

--- formation_indus_ds_avancee/train_and_predict.py
+++ formation_indus_ds_avancee/train_and_predict.py
@@ -5,12 +5,10 @@ import joblib
 import pandas as pd
 from sklearn.ensemble import RandomForestRegressor
 
-
 def train_model_with_io(features_path: str, model_registry_folder: str) -> None:
     features = pd.read_parquet(features_path)
 
     train_model(features, model_registry_folder)
-
 
 def train_model(features: pd.DataFrame, model_registry_folder: str) -> None:
     target = 'Ba_avg'
@@ -30,7 +28,6 @@ def predict_with_io(features_path: str, model_path: str, predictions_folder: str
     features[['predictions', 'predictions_time']].to_csv(os.path.join(predictions_folder, time_str + '.csv'),
                                                          index=False)
     features[['predictions', 'predictions_time']].to_csv(os.path.join(predictions_folder, 'latest.csv'), index=False)
-
 
 def predict(features: pd.DataFrame, model_path: str) -> pd.DataFrame:
     model = joblib.load(model_path)
